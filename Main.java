939e5c3db8e2c4fcfca5dd3fa66b83c5c3ec9944
LyreRabbit <1079384564@qq.com>
2018-06-25 12:23:37 +0800

    上传Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+package ms;
+
+import javax.swing.*;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * ClassName:Main<br>
+ * Description£ºÊµÀý»¯Images¡¢Mine.
+ * @version 1.0
+ */
+public class Main {
+	public static void main(String[] args)
+    {
+    	new Images(); 
+		new Mine();
+    }
+
+}
